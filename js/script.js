288a55bb27438177bc1db2ff5d99992bd3319aaf
cquentin26200 <qcheron@edenschool.fr>
2022-04-21 16:38:32 +0200

    add content and design for header

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-const SomeElement = $("div.someelement");
-$("body").append(SomeElement)
+$("body").css("margin", "0");
+
+const $header = $("<header></header>").appendTo('body');
+$header.css("background", "#40a0ff");
+$header.css("textAlign", "center");
+$header.css("padding", "2rem");
+$header.css("textTransform", "uppercase");
+
+const $title = $("<h1>kanye west</h1>").appendTo('header')
+$title.css("marginTop", "0");
+
+const $paragraphInHeader = $("<p>welcome to the best quote app</p>").appendTo('header');
+
+const $buttonInHeader = $("<button>add quote</button>").appendTo('header');
+$buttonInHeader.css("textTransform", "uppercase");
