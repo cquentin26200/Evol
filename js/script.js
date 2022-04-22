8a7852b64ccfe58c92bdb19a5351c239bccd577e
cquentin26200 <qcheron@edenschool.fr>
2022-04-22 14:38:06 +0200

    add list

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-$("body").css("margin", "0");
-
-const $header = $("<header></header>").appendTo('body');
-$header.css("background", "#40a0ff");
-$header.css("textAlign", "center");
-$header.css("padding", "2rem");
-$header.css("textTransform", "uppercase");
-
-const $title = $("<h1>kanye west</h1>").appendTo('header')
-$title.css("marginTop", "0");
-
-const $paragraphInHeader = $("<p>welcome to the best quote app</p>").appendTo('header');
-
-const $buttonInHeader = $("<button>add quote</button>").appendTo('header');
-$buttonInHeader.css("textTransform", "uppercase");
-
-const $main = $("<main></main>").appendTo("body");
-$main.css("maxWidth", "700px");
-$main.css("margin", "0 auto");
-
 async function getApi () {
     return fetch("https://api.kanye.rest")
         .then(response => response.json())
@@ -26,13 +6,15 @@ async function getApi () {
 
 getApi()
 
+const $main = $("main");
+
 const $ul = $("<ul></ul>").appendTo("main");
 $ul.css("listStyle", "none");
 
 async function useApi () {
     const $sentence = await getApi();
 
-    const $li = $(`<li>${$sentence}</li>`).appendTo("ul");
+    const $li = $(`<li><span>${$sentence}</span></li>`).appendTo("ul");
     $li.addClass("allLi");
     $li.css("padding", "2rem");
     $li.css("border", "1px solid black");
@@ -40,23 +22,12 @@ async function useApi () {
     $li.css("display", "flex");
     $li.css("flexDirection", "column");
 
-    const $up = $(`<button>up</button>`).appendTo("li");
+    const $up = $(`<button>up</button>`)
     $up.addClass("allUp");
     $up.css("width", "2rem");
-    $up.css("left", "38.2rem")
-    $up.css("position", "relative");
-    $up.css("bottom", "1rem");
+    $("li").last().append($up)
 
-    const $down = $(`<button>down</button>`).appendTo("li");
-    $down.addClass("allUp");
-    $down.css("width", "3rem");
-    $down.css("left", "37.7rem")
-    $down.css("position", "relative");
-    $down.css("bottom", "1rem");
-
-   $("ul").each(function() {
-       $(this).css("listStyle", "none");
-   });
+    
 }
 
-useApi()
+$("header button").on("click", useApi);
